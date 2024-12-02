baa4bb7d02c68fc07cd145ba660c9000d856e3c0
Lex Christopherson <lexchristopherson@Lexs-MacBook-Pro.local>
2024-12-02 16:21:42 -0600

    Add InTheStudio page and route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import SamplePack from './components/SamplePack';
+import InTheStudio from './components/InTheStudio';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sample-pack" element={<SamplePack />} />
-      </Routes>
+      <Switch>
+        <Route path="/in-the-studio" component={InTheStudio} />
+        <Route path="/sample-pack" component={SamplePack} />
+        <Route path="/" component={Home} />
+      </Switch>
     </Router>
   );
 }
--- src/components/SamplePack.js
+++ src/components/SamplePack.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 const PageContainer = styled.div`
   min-height: 100vh;
   width: 100vw;
   background: #1a1a1a;
-  background-image: url('/assets/images/sample-pack-background.jpg');
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
   color: white;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   overflow: hidden;
   position: relative;
 
@@ -79,15 +76,39 @@ const ButtonContainer = styled.div`
 `;
 
 function SamplePack() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://link.glittercowboy.com/js/form_embed.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <PageContainer>
       <ContentWrapper>
-        <CoverImage 
-          src="/assets/images/sample-pack-cover.jpg" 
-          alt="PIANIKA Sample Pack Cover"
-        />
-        <ButtonContainer>
-        </ButtonContainer>
+        <div style={{ width: '100%', maxWidth: '600px', height: '430px' }}>
+          <iframe
+            src="https://link.glittercowboy.com/widget/form/x4tXspKfOaqNOMLEzit5"
+            style={{ width: '100%', height: '100%', border: 'none', borderRadius: '3px' }}
+            id="inline-x4tXspKfOaqNOMLEzit5"
+            data-layout="{'id':'INLINE'}"
+            data-trigger-type="alwaysShow"
+            data-trigger-value=""
+            data-activation-type="alwaysActivated"
+            data-activation-value=""
+            data-deactivation-type="neverDeactivate"
+            data-deactivation-value=""
+            data-form-name="In The Studio With: PIANIKA"
+            data-height="430"
+            data-layout-iframe-id="inline-x4tXspKfOaqNOMLEzit5"
+            data-form-id="x4tXspKfOaqNOMLEzit5"
+            title="In The Studio With: PIANIKA"
+          ></iframe>
+        </div>
       </ContentWrapper>
     </PageContainer>
   );
